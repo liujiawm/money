b0121520a0c49dbcb1fa7c22d2ce2721ed081842
phpu <liujiawm@163.com>
2020-02-28 19:30:35 +0800

    重新调整

--- money.go
+++ money.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/shopspring/decimal"
 )
@@ -15,23 +16,22 @@ import (
 // 同理比特币的最小单位是Satoshi
 type Munit int64
 
-
 // type Amount struct {
 // 	val Munit
 // }
 
 type Money struct {
-	amount Munit
+	amount   Munit
 	currency *Currency
+	error    error
+
+	mu sync.RWMutex
 }
 
 const (
 	minMunit Munit = math.MinInt64
 	maxMunit Munit = math.MaxInt64
 )
-
-
-
 
 // const (
 // 	Milli  Munit = 1 // 钱的最小单位 毫
@@ -41,100 +41,257 @@ const (
 // 	Yuan         = 10 * Jiao  // 元 Dollar
 // )
 
-// Munit是否超出maxMunit与minMunit的范围
-func validMunit(v Munit) error{
-	if v > maxMunit || v < minMunit {
-		return errors.New("溢出资金范围")
-	}
-	return nil
+func (m Munit) String() string {
+	return strconv.FormatInt(int64(m), 10)
 }
+
 // Decimal是否超出maxMunit与minMunit的范围
 func decimalValidMunit(v decimal.Decimal) error {
-	if v.GreaterThan(maxMunit.munitToDecimal())==true || v.LessThan(minMunit.munitToDecimal()) {
+	if v.GreaterThan(maxMunit.munitToDecimal()) == true || v.LessThan(minMunit.munitToDecimal()) {
 		return errors.New("溢出资金范围")
 	}
 	return nil
 }
 
-func (m Munit)munitToDecimal() decimal.Decimal {
-	return decimal.New(int64(m),0)
+func (m Munit) munitToDecimal() decimal.Decimal {
+	return decimal.New(int64(m), 0)
 }
 
+//     -1 if m <  m2
+//      0 if m == m2
+//     +1 if m >  m2
+//
+func (m Munit) Cmp(m2 Munit) int {
+	return m.munitToDecimal().Cmp(m2.munitToDecimal())
+}
+
+// m=m2
+func (m Munit) Equal(m2 Munit) bool {
+	return m.Cmp(m2) == 0
+}
+
+// m > m2
+func (m Munit) GreaterThan(m2 Munit) bool {
+	return m.Cmp(m2) == 1
+}
+
+// m >= m2
+func (m Munit) GreaterThanOrEqual(m2 Munit) bool {
+	cmp := m.Cmp(m2)
+	return cmp == 1 || cmp == 0
+}
+
+// m < m2
+func (m Munit) LessThan(m2 Munit) bool {
+	return m.Cmp(m2) == -1
+}
+
+// m <= m2
+func (m Munit) LessThanOrEqual(m2 Munit) bool {
+	cmp := m.Cmp(m2)
+	return cmp == -1 || cmp == 0
+}
+
+// 是否为0
+func (m Munit) IsZero() bool {
+	return m.munitToDecimal().IsZero()
+}
+
+// 绝对值
+func (m Munit) Abs() (result Munit, err error) {
+	if md := m.munitToDecimal(); md.IsZero() == true {
+		return 0, nil
+	} else {
+		md = md.Abs()
+		if err = decimalValidMunit(md); err != nil {
+			return 0, err
+		}
+		result = Munit(md.IntPart())
+
+		return result, nil
+	}
+}
+
+// 负 returns -m
+func (m Munit) Neg() (result Munit, err error) {
+	if md := m.munitToDecimal(); md.IsZero() == true {
+		return 0, nil
+	} else {
+		md = md.Neg()
+		if err = decimalValidMunit(md); err != nil {
+			return 0, err
+		}
+		result = Munit(md.IntPart())
+
+		return result, nil
+	}
+}
 
 // 加，如果结果超过范围限制err != nil
-func (m Munit)Add(m2 Munit) (result Munit, err error){
+func (m Munit) Add(m2 Munit) (result Munit, err error) {
 	md := m.munitToDecimal().Add(m2.munitToDecimal())
 	if err = decimalValidMunit(md); err != nil {
-		return 0,err
+		return 0, err
 	}
 	result = Munit(md.IntPart())
 
-	return result,nil
+	return result, nil
 }
+
 // 减，如果结果超过范围限制err != nil
-func (m Munit)Sub(m2 Munit) (result Munit, err error){
+func (m Munit) Sub(m2 Munit) (result Munit, err error) {
 	md := m.munitToDecimal().Sub(m2.munitToDecimal())
 	if err = decimalValidMunit(md); err != nil {
-		return 0,err
+		return 0, err
 	}
 	result = Munit(md.IntPart())
 
-	return result,nil
+	return result, nil
 }
 
-// 除，m/m2 因为是最小单位，所以结果四舍五入返回整数
-func (m Munit)Div(m2 Munit) (result Munit, err error){
-	md := m.munitToDecimal().DivRound(m2.munitToDecimal(),0)
+// 乘，m * v 因为是最小单位，所以结果四舍五入返回整数
+func (m Munit) Mul(v int) (result Munit, err error) {
+	md := m.munitToDecimal().Mul(decimal.NewFromInt(int64(v)))
 	if err = decimalValidMunit(md); err != nil {
-		return 0,err
+		return 0, err
 	}
 	result = Munit(md.IntPart())
 
-	return result,nil
+	return result, nil
+}
+
+// 除，m/v 因为是最小单位，所以结果四舍五入返回整数
+func (m Munit) Div(v int) (result Munit, err error) {
+	md := m.munitToDecimal().DivRound(decimal.NewFromInt(int64(v)), 0)
+	if err = decimalValidMunit(md); err != nil {
+		return 0, err
+	}
+	result = Munit(md.IntPart())
+
+	return result, nil
 }
 
 func New(v Munit, code string) *Money {
-	return &Money{
-		amount: v,
-		currency: newCurrency(code).get(),
-	}
+	m := new(Money)
+	currency := newCurrency(code).get()
+	m.set(v, currency, nil)
+	return m
 }
-func (m *Money)GetAmount()Munit{
+
+func (m *Money) set(v Munit, currency *Currency, e error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.amount = v
+	m.currency = currency
+	m.error = e
+}
+
+func (m *Money) setAmount(v Munit) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.amount = v
+}
+
+func (m *Money) setError(e error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.error = e
+}
+
+func (m *Money) GetAmount() Munit {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	return m.amount
 }
-func (m *Money)GetCurrency()*Currency{
+func (m *Money) GetCurrency() *Currency {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	return m.currency
 }
 
-func (m *Money)MoneyAdd(m2 *Money) (*Money,error) {
+func (m *Money) GetError() error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return m.error
+}
+
+func (m *Money) MoneyAdd(m2 *Money) error {
 	if m.currency.Code != m2.currency.Code {
-		return nil,errors.New("币种不同不能相加")
+		return errors.New("币种不同不能相加")
 	}
-	mm,err := m.amount.Add(m2.amount)
+	mm, err := m.amount.Add(m2.amount)
 	if err != nil {
-		return nil,err
+		return err
+	}
+
+	m.setAmount(mm)
+	return nil
+}
+
+func MoneyAdd(m, m2 *Money) (*Money, error) {
+	if m.currency.Code != m2.currency.Code {
+		return nil, errors.New("币种不同不能计算")
+	}
+	mm, err := m.amount.Add(m2.amount)
+	if err != nil {
+		return nil, err
 	}
 	return &Money{
-		amount:mm,
-		currency:m.currency,
-	},nil
+		amount:   mm,
+		currency: m.currency,
+		error:    nil,
+	}, nil
+	// nm := new(Money)
+	// nm.set(mm,m.currency,nil)
+	// return nm, nil
+}
 
+func (m *Money) MoneySub(m2 *Money) error {
+	if m.currency.Code != m2.currency.Code {
+		return errors.New("币种不同不能计算")
+	}
+	mm, err := m.amount.Sub(m2.amount)
+	if err != nil {
+		return err
+	}
+
+	m.setAmount(mm)
+	return nil
+}
+
+func MoneySub(m, m2 *Money) (*Money, error) {
+	if m.currency.Code != m2.currency.Code {
+		return nil, errors.New("币种不同不能相加")
+	}
+	mm, err := m.amount.Sub(m2.amount)
+	if err != nil {
+		return nil, err
+	}
+	nm := new(Money)
+	nm.set(mm, m.currency, nil)
+	return nm, nil
 }
 
 // 转小数
 // per int 转小数的除数值
 // pr int32 保留的小数位数
 // asbank bool 是否用银行的四舍五入方式取数，详见decimal.RoundBank
-func (m *Money)round(per int, pr int32, asbank bool)string{
+func (m *Money) round(per int, pr int32, asbank bool) string {
 	if m.amount == 0 {
 		return "0"
 	}
 
-	if pr == 0 ||  per == 0 {
-		return strconv.FormatInt(int64(m.amount),10)
+	if pr == 0 || per == 0 {
+		return strconv.FormatInt(int64(m.amount), 10)
 	}
 
-	d := m.amount.munitToDecimal().DivRound(decimal.NewFromInt(int64(per)),pr+1)
+	d := m.amount.munitToDecimal().DivRound(decimal.NewFromInt(int64(per)), pr+1)
 	ds := ""
 	if asbank {
 		ds = d.StringFixedBank(pr)
@@ -142,31 +299,29 @@ func (m *Money)round(per int, pr int32, asbank bool)string{
 		ds = d.StringFixed(pr)
 	}
 	if m.currency.DecimalMark != "." {
-		return strings.Replace(ds,".",m.currency.DecimalMark,1)
+		return strings.Replace(ds, ".", m.currency.DecimalMark, 1)
 	}
 	return ds
 }
-
 
 // 转小数,用银行的四舍五入方式取数
 // per int 转小数的除数值
 // pr int32 保留的小数位数
 // 详见decimal.RoundBank
-func (m *Money)RoundBank(per int ,pr int32)string{
-	return m.round(per,pr,true)
+func (m *Money) RoundBank(per int, pr int32) string {
+	return m.round(per, pr, true)
 }
 
 // 转小数，四舍五入
 // per int 转小数的除数值
 // pr int32 保留的小数位数
-func (m *Money)Round(per int ,pr int32)string{
-	return m.round(per,pr,false)
+func (m *Money) Round(per int, pr int32) string {
+	return m.round(per, pr, false)
 }
-
 
 // 最小单位转标准单位(小数字符串)
 // asbank是否用银行的四舍五入方式取数，详见decimal.RoundBank
-func (m *Money)AsMajorUnits(asbank bool) string{
+func (m *Money) AsMajorUnits(asbank bool) string {
 
 	pr := m.currency.precision()
 
@@ -179,13 +334,13 @@ func (m *Money)AsMajorUnits(asbank bool) string{
 
 // 千分分隔
 // asbank是否用银行的四舍五入方式取数，详见decimal.RoundBank
-func (m *Money)ThousandsSeparator(asbank bool)string{
+func (m *Money) ThousandsSeparator(asbank bool) string {
 	ds := m.AsMajorUnits(asbank)
-	if l := strings.LastIndex(ds,m.currency.DecimalMark); l > 0  && len(m.currency.ThousandsSeparator) > 0 {
+	if l := strings.LastIndex(ds, m.currency.DecimalMark); l > 0 && len(m.currency.ThousandsSeparator) > 0 {
 		dsIntPart := ds[:l]
 		dsDecimalPart := ds[l:]
 
-		for i := len(dsIntPart)-3; i>0;i-=3 {
+		for i := len(dsIntPart) - 3; i > 0; i -= 3 {
 			dsIntPart = dsIntPart[:i] + m.currency.ThousandsSeparator + dsIntPart[i:]
 		}
 		return dsIntPart + dsDecimalPart
--- money_test.go
+++ money_test.go
@@ -3,64 +3,140 @@ package money
 import (
 	"fmt"
 	"testing"
-
 )
 
-func TestAdd(t *testing.T){
+var (
+	a Munit = 8425896210
+	b Munit = 5456789123
+	c Munit = 789461244567123453
 
-	var a,b,c Munit = 45896210,456789123,789461244567123453
+	ma = New(a, "CNY")
+	mb = New(b, "CNY")
+	mc = New(c, "BTC")
+)
 
-	ma:=New(a,"CNY")
-	mb:=New(b,"CNY")
-	mc:=New(c,"BTC")
+func TestMunitCompare(t *testing.T) {
+	fmt.Println("************************** 两值比较 **************************")
+	cm := a.Cmp(b)
+	if cm != 1 {
+		t.Error("比较错误")
+	}
+	// ==
+	if a.Equal(b) {
+		t.Error("比较错误")
+	}
 
-	fmt.Println("//////////////////////////////////////////////////////////////")
+	// >
+	if b.GreaterThan(a) {
+		t.Error("比较错误")
+	}
+
+	// >=
+	if b.GreaterThanOrEqual(a) {
+		t.Error("比较错误")
+	}
+
+	// <
+	if a.LessThan(b) {
+		t.Error("比较错误")
+	}
+	// <=
+	if a.LessThanOrEqual(b) {
+		t.Error("比较错误")
+	}
+
+}
+
+func TestMunitCalculate(t *testing.T) {
+	fmt.Println("************************** 两值计算 **************************")
+
 	fmt.Println("Munit相加")
-	ab,err := a.Add(b)
+	ab, err := a.Add(b)
 	if err != nil {
 		fmt.Println(err.Error())
-	}else {
-		fmt.Println(ab)
-		fmt.Printf("%d加%d等于%d\n",int64(a),int64(b),int64(ab))
+	} else {
+		fmt.Printf("%v加%v等于%v\n", a, b, ab)
+	}
+	fmt.Println("Munit相减")
+	ab, err = a.Sub(b)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%v减%v等于%v\n", a, b, ab)
 	}
 
-	fmt.Println("//////////////////////////////////////////////////////////////")
+	fmt.Println("Munit乘5")
+	mul, err := a.Mul(5)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		// mul == 42129481050
+		fmt.Printf("%v乘%v等于%v\n", a, 5, mul)
+	}
+
+	fmt.Println("Munit除以5")
+	div, err := a.Div(5)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		// div == 1685179242
+		fmt.Printf("%v除以%v等于%v\n", a, 5, div)
+	}
+}
+
+func TestMoneyCalculate(t *testing.T) {
+	fmt.Println("************************** 钱计算 **************************")
+
 	fmt.Println("相同币种的Money相加")
-	mamb,err := ma.MoneyAdd(mb)
+	mamb, err := MoneyAdd(ma, mb)
 	if err != nil {
 		fmt.Println(err.Error())
-	}else {
-		fmt.Printf("%d(%s)加%d(%s)等于%d(%s)\n",int64(ma.GetAmount()),ma.GetCurrency().Code,int64(mb.GetAmount()),mb.GetCurrency().Code,int64(mamb.GetAmount()),mamb.GetCurrency().Code)
+	} else {
+		fmt.Printf("%d(%s)加%d(%s)等于%d(%s)\n", int64(ma.GetAmount()), ma.GetCurrency().Code, int64(mb.GetAmount()), mb.GetCurrency().Code, int64(mamb.GetAmount()), mamb.GetCurrency().Code)
 	}
 
-	fmt.Println("//////////////////////////////////////////////////////////////")
 	fmt.Println("不同币种的Money相加")
-	mamc,err := ma.MoneyAdd(mc)
-	if err != nil {
+	if err := ma.MoneyAdd(mc); err != nil {
 		fmt.Println(err.Error())
-	}else {
-		fmt.Printf("%d(%s)加%d(%s)等于%d(%s)\n",int64(ma.GetAmount()),ma.GetCurrency().Code,int64(mc.GetAmount()),mc.GetCurrency().Code,int64(mamc.GetAmount()),mamc.GetCurrency().Code)
+	} else {
+		t.Error("Money计算错误")
 	}
 
+	//ch := make(chan bool)
+	go func() {
+		fmt.Println("相加后附给ma")
+		if err := ma.MoneyAdd(mb); err != nil {
+			fmt.Println(err.Error())
+		}
+		//ch <- true
+	}()
+	//<-ch
+
+	fmt.Println("相同币种的Money相减")
+	mamb, err = MoneySub(ma, mb)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%d(%s)减%d(%s)等于%d(%s)\n", int64(ma.GetAmount()), ma.GetCurrency().Code, int64(mb.GetAmount()), mb.GetCurrency().Code, int64(mamb.GetAmount()), mamb.GetCurrency().Code)
+	}
+
+	mamb, err = MoneySub(mb, ma)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%d(%s)减%d(%s)等于%d(%s)\n", int64(mb.GetAmount()), mb.GetCurrency().Code, int64(ma.GetAmount()), ma.GetCurrency().Code, int64(mamb.GetAmount()), mamb.GetCurrency().Code)
+	}
 
 }
 
-func TestAsMajorUnits(t *testing.T){
-	var m Munit =8223372036854775855
-	mm := New(m,"CNY")
-
-	fmt.Println("//////////////////////////////////////////////////////////////")
-	fmt.Println(m)
-	fmt.Println("转标准单位")
-	fmt.Println(mm.AsMajorUnits(true))
+func TestAsMajorUnits(t *testing.T) {
+	fmt.Println("************************** 转标准单位 **************************")
+	fmt.Println(a)
+	fmt.Println(ma.AsMajorUnits(true))
 }
 
-func TestThousandsSeparator(t *testing.T){
-	var m Munit =8223372036854775855
-	mm := New(m,"CNY")
-
-	fmt.Println("//////////////////////////////////////////////////////////////")
-	fmt.Println(m)
-	fmt.Println("转标准单位后千分分隔")
-	fmt.Println(mm.ThousandsSeparator(false))
+func TestThousandsSeparator(t *testing.T) {
+	fmt.Println("************************** 转标准单位后千分分隔 **************************")
+	fmt.Println(c)
+	fmt.Printf("%s %s \n", mc.GetCurrency().Symbol, mc.ThousandsSeparator(false))
 }
